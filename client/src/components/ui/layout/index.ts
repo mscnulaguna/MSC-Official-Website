97a77d57b5b16e17fb2c6cbc97a2b449a099419c
sonickysh <keithyvesrobles@gmail.com>
2026-05-01 01:48:47 +0800

    fix missing admin pages

--- client/src/components/ui/layout/index.ts
+++ client/src/components/ui/layout/index.ts
@@ -7,6 +7,7 @@
 export { Navbar } from './Navbar'
 export { Footer } from './Footer'
 export { Layout } from './Layout'
+export { AdminLayout } from './AdminLayout'
 export { SearchDialog } from './SearchDialog'
 export { MobileNavDrawer } from './MobileNavDrawer'
 export { NavbarLeft } from './NavbarLeft'
